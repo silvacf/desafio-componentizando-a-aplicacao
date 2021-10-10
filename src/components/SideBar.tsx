a400be08ae26853d4662d80d2ba0449626909d6e
camilla <camferreiradasilva@gmail.com>
2021-10-10 19:57:47 -0300

    primeira versão

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface MovieGenres{
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface sideBarProps{
+  moviesGenres: MovieGenres[];
+  selectedGenreId:number;
+  handleClickButton:any;
+}
+
+export function SideBar(props:sideBarProps) {
+  return(
+     <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {props.moviesGenres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => props.handleClickButton(genre.id)}
+              selected={props.selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+  )
 }
